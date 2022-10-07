e82ecdd2f48a36e7f0330e3706c303f6bd3c2899
amador_jose <amadorjose@gmail.com>
2022-10-07 15:24:42 -0300

    Jose: modifiaciones en la AppBlog/views.py + Ajustes necesarios para mostrar las propiedades del modelo Posteo en la vista

--- AppBlog/views.py
+++ AppBlog/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.utils import timezone
+from .models import Posteo
+
+
+
 def lista_posteos(request):
-    return render(request, 'html/lista_posteos.html', {})
+    posteos = Posteo.objects.filter(fecha_publicado__lte=timezone.now()).order_by('fecha_creado')
+    return render(request, 'html/lista_posteos.html', {'posteos':posteos})
+
