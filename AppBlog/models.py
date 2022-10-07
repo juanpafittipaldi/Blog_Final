222edcc172fb8f40441143585a779acb79fa5821
amador_jose <amadorjose@gmail.com>
2022-10-07 13:41:29 -0300

    Jose: Creacion y Migracion de BBDD y Blog/settings.py. Armado del modelo Posteo en AppBlog/models.py

--- AppBlog/models.py
+++ AppBlog/models.py
@@ -1,3 +1,20 @@
+
 from django.db import models
 
-# Create your models here.
+from django.conf import settings
+from django.utils import timezone
+
+
+class Posteo(models.Model):
+    autor = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    titulo = models.CharField(max_length=500)
+    contenido = models.TextField()
+    fecha_creado = models.DateTimeField(default=timezone.now)
+    fecha_publicado = models.DateTimeField(blank=True, null=True)
+
+    def publicar(self):
+        self.fecha_publicado = timezone.now()
+        self.save()
+
+    def __str__(self):
+        return self.titulo
