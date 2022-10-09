4a22d861e5b8e1a40c52bda5cca3d64e9b43e009
amador_jose <amadorjose@gmail.com>
2022-10-09 19:46:59 -0300

    Jose: POR RESOLVER = Persiste el problema con la subida de las imagenes. Se agregó request.FILE a todas las instancias de PosteoForm en views.py

--- AppBlog/views.py
+++ AppBlog/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, get_object_or_404, redirect
 from django.utils import timezone
 from .forms import PosteoForm
 from .models import Posteo
+from django.contrib.auth.decorators import login_required
 #Read
 def lista_posteos(request):
     posteos = Posteo.objects.filter(fecha_publicado__lte=timezone.now()).order_by('-fecha_creado')
@@ -11,10 +12,13 @@ def lista_posteos(request):
 def detalle_posteo(request, pk):
     posteo = get_object_or_404(Posteo, pk=pk)
     return render(request, 'html/detalle_posteo.html', {'posteo': posteo})
+
+@login_required
+
 #Create
 def posteo_nuevo(request):
     if request.method == "POST":
-        form = PosteoForm(request.POST)
+        form = PosteoForm(request.POST, request.FILES)
         if form.is_valid():
             posteo = form.save(commit=False)
             posteo.autor = request.user
@@ -23,11 +27,13 @@ def posteo_nuevo(request):
     else:
         form = PosteoForm()
     return render(request, 'html/editar_posteo.html', {'form': form})
+
+@login_required
 #Update
 def editar_posteo(request, pk):
     posteo = get_object_or_404(Posteo, pk=pk)
     if request.method == "POST":
-        form = PosteoForm(request.POST, instance=posteo)
+        form = PosteoForm(request.POST, request.FILES,instance=posteo)
         if form.is_valid():
             post = form.save(commit=False)
             post.autor = request.user
@@ -36,15 +42,19 @@ def editar_posteo(request, pk):
     else:
         form = PosteoForm(instance=posteo)
     return render(request, 'html/editar_posteo.html', {'form': form})
+
+@login_required
 #Read
 def posteo_borradores(request):
     posteos = Posteo.objects.filter(fecha_publicado__isnull=True).order_by('-fecha_creado')
     return render(request, 'html/posteo_borradores.html', {'posteos': posteos})
+@login_required
 #Create
 def publicar_posteo(request, pk):
     posteo = get_object_or_404(Posteo, pk=pk)
     posteo.publicar()
     return redirect('lista_posteos')
+@login_required
 #Delete
 def eliminar_posteo(request, pk):
     posteo = get_object_or_404(Posteo, pk=pk)
